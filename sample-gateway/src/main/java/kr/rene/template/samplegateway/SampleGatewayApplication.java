5b9ac4e9d07136f803630a5ae5b6793050d15870
renechoi <115696395+renechoi@users.noreply.github.com>
2023-12-19 08:28:51 +0900

    feat: initial commit

---
+++ sample-gateway/src/main/java/kr/rene/template/samplegateway/SampleGatewayApplication.java
@@ -0,0 +1,13 @@
+package kr.rene.template.samplegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleGatewayApplication.class, args);
+	}
+
+}
---
+++ sample-gateway/src/test/java/kr/rene/template/samplegateway/SampleGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.rene.template.samplegateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
