eb7e2f7ea687c2ae32a516178e33accd937943dc
renechoi <115696395+renechoi@users.noreply.github.com>
2023-12-21 19:31:03 +0900

    feat: annotation 설정

---
+++ sample-domain-1/src/main/java/kr/rene/template/sampledomain1/common/annotation/Factory.java
@@ -0,0 +1,22 @@
+package kr.rene.template.sampledomain1.common.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author : Rene Choi
+ * @since : 2023/12/19
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Component
+public @interface Factory {
+}
